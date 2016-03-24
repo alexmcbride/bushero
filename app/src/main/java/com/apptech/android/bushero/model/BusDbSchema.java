b2a6289ead3445647cfae900916d565215dc5a67
Alex McBride <xandermcbride@gmail.com>
2016-03-24 11:07:25 +0000

    fixed untracked files

--- app/src/main/java/com/apptech/android/bushero/model/BusDbSchema.java
+++ app/src/main/java/com/apptech/android/bushero/model/BusDbSchema.java
@@ -70,4 +70,15 @@ public class BusDbSchema {
             public static final String SOURCE = "source";
         }
     }
+
+    public class FavouriteStopTable {
+        public static final String NAME = "FavouriteStop";
+
+        public class Columns {
+            public static final String ID = "id";
+            public static final String ATCOCODE = "atcoCode";
+            public static final String LONGITUDE = "longitude";
+            public static final String LATITUDE = "latitude";
+        }
+    }
 }
